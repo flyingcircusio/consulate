db394aa924383a48459f2001796012fe2c747f18
Christian Theune <ct@flyingcircus.io>
2023-02-06 11:15:37 +0100

    Back to development: 1.1.0+fc3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='consulate',
-    version='1.1.0+fc2',
+    version='1.1.0+fc3.dev0',
     description='A Client library and command line application for the Consul',
     maintainer='Gavin M. Roy',
     maintainer_email='gavinr@aweber.com',
